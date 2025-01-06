c664c79912f4d4771ef1e6b8806914f6ec6e2946
deniisolo <deniisolo@outlook.es>
2025-01-06 16:07:36 -0500

    feat(DriverController): add Drivercontroller and some validation

--- force-app/main/default/lwc/activeDrivers/activeDrivers.js
+++ force-app/main/default/lwc/activeDrivers/activeDrivers.js
@@ -1,28 +1,15 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getActiveDrivers from '@salesforce/apex/DriverController.getActiveDrivers';
 
 export default class ActiveDrivers extends LightningElement {
-    // Datos simulados de los pilotos
-    drivers = [
-        {
-            Id: '001',
-            Name: 'Max Verstappen',
-            Team: 'Red Bull Racing',
-            Sponsor: 'Oracle',
-            ImageUrl: '/resource/User',
-        },
-        {
-            Id: '002',
-            Name: 'Lewis Hamilton',
-            Team: 'Mercedes',
-            Sponsor: 'Petronas',
-            ImageUrl: '/resource/User',
-        },
-        {
-            Id: '003',
-            Name: 'Charles Leclerc',
-            Team: 'Ferrari',
-            Sponsor: 'Shell',
-            ImageUrl: '/resource/User',
-        },
-    ];
+    drivers;
+
+    @wire(getActiveDrivers)
+    wiredDrivers({ error, data }) {
+        if (data) {
+            this.drivers = data;
+        } else if (error) {
+            console.error('Error fetching drivers:', error);
+        }
+    }
 }
